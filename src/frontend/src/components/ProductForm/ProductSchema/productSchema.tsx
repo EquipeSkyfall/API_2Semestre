477aa8bf18e53976749a327f382480641341988c
Eduardo Fontes <160733714+DuuhZero@users.noreply.github.com>
2024-10-12 00:24:39 -0300

    Tradução dos avisos de productSchema

--- src/frontend/src/components/ProductForm/ProductSchema/productSchema.tsx
+++ src/frontend/src/components/ProductForm/ProductSchema/productSchema.tsx
@@ -2,17 +2,17 @@
 import { z } from 'zod';
 
 export const productSchema = z.object({
-    nome_produto: z.string().min(1, "Product name is required"),
+    nome_produto: z.string().min(1, "Nome do Produto é obrigatório"),
     descricao_produto: z.string().optional(),
     marca_produto: z.string().optional(),
     modelo_produto: z.string().optional(),
-    preco_venda: z.number().min("Invalid price.").positive(),
-    altura_produto: z.number().min("Invalid height.").positive(),
-    largura_produto: z.number().min("Invalid width.").positive(),
-    comprimento_produto: z.number().min("Invalid depth.").positive(),
+    preco_venda: z.number().min("Preço Invalido.").positive(),
+    altura_produto: z.number().min("Largura Invalida.").positive(),
+    largura_produto: z.number().min("Altura Invalida.").positive(),
+    comprimento_produto: z.number().min("Profundidade Invalida.").positive(),
     localizacao_estoque: z.string().optional(),
     permalink_imagem: z.string().optional(),
-    peso_produto: z.number().min("Invalid weight.").positive(),
+    peso_produto: z.number().min("Peso Invalido.").positive(),
     id_categoria: z.preprocess(
         (value) => (value === '' ? null : Number(value)),
         z.number().optional().nullable()
