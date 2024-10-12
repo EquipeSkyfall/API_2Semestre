64aae90a87a7ee9f527a98e962fa01f1620b3428
Eduardo Fontes <160733714+DuuhZero@users.noreply.github.com>
2024-10-12 00:14:43 -0300

    Tradução de avisos do minimo necessário a ser cadastrado

--- src/frontend/src/components/ProductForm/ProductSchema/productSchema.tsx
+++ src/frontend/src/components/ProductForm/ProductSchema/productSchema.tsx
@@ -2,13 +2,13 @@
 import { z } from 'zod';
 
 export const productSchema = z.object({
-    product_name: z.string().min(1, "Product name is required"),
-    batch: z.string().min(1, "Batch is required"),
+    product_name: z.string().min(1, "O nome do Produto é obrigatório"),
+    batch: z.string().min(1, "O lote é obrigatório"),
     description: z.string().optional(),
     brand: z.string(),
-    quantity: z.number().min(1, "Quantity must be at least 1").positive(),
-    price: z.number().min(1, "Price must be at least 1").positive(),
-    retail_price: z.number().min(1, "Retail price must be at least 1").positive(),
+    quantity: z.number().min(1, "Quantidade deve ter pelo menos 1").positive(),
+    price: z.number().min(1, "Preço deve ter pelo menos 1").positive(),
+    retail_price: z.number().min(1, "Preço de Venda deve ter pelo menos 1").positive(),
     stock_location: z.string().optional().nullable(),
     id_category: z.preprocess(
         (value) => (value === undefined ? undefined : Number(value)),
