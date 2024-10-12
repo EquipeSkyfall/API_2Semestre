c9862b71e3373ccf8a9e2e01b8d8d61aca61b9a3
Eduardo Fontes <160733714+DuuhZero@users.noreply.github.com>
2024-10-12 00:22:20 -0300

    NavBar atualizada com estilo novo

--- src/frontend/src/components/NavBar/index.tsx
+++ src/frontend/src/components/NavBar/index.tsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-export default function NavBar(){
-    return(
-        <nav className="bg-slate-600 flex justify-around text-white">
-          <Link to='/'><button >Home</button></Link>
-          <Link to='/products'><button >Produtos</button></Link>
-          <Link to='/teste'><button >Histórico</button></Link>
-          <Link to='/teste'><button >Movimentações</button></Link>
-          <Link to='/teste'><button >Relatórios</button></Link>
-        </nav>
-    )
-
+export default function NavBar() {
+  return (
+    <nav className="bg-neutral-100 flex items-center gap-12 p-4 mb-5 text-black border-b-slate-100 pb-0 -mt-2">
+      <img src="/Prostock.png" width={120}  className=" -mt-10 -mb-5" alt="Logo" />
+      <Link to='/products'>
+        <button className="hover:border-b-4 hover:border-b-cyan-400 pb-2 bordernav">Produtos</button>
+      </Link>
+      <Link to='/teste'>
+        <button className="hover:border-b-4 hover:border-b-cyan-400 pb-2 bordernav">Histórico</button>
+      </Link>
+      <Link to='/teste'>
+        <button className="hover:border-b-4 hover:border-b-cyan-400 pb-2 bordernav">Movimentações</button>
+      </Link>
+      <Link to='/teste'>
+        <button className="hover:border-b-4 hover:border-b-cyan-400 pb-2 bordernav">Relatórios</button>
+      </Link>
+    </nav>
+  );
 }
