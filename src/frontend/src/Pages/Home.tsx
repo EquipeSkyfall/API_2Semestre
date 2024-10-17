d8cf942531ad463995bd4e5ec86c09869014fc82
Eduardo Fontes <160733714+DuuhZero@users.noreply.github.com>
2024-10-17 16:22:43 -0300

    Adição da font no estilo da pagina

--- src/frontend/src/Pages/Home.tsx
+++ src/frontend/src/Pages/Home.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     return (
         <>
             <div className="text-center">
-                <img src="/Prostock.png" alt="" className=" absolute w-52 left-1/2 right-96 transform -translate-x-28 -translate-y-1/2 opacity-35" />
-                <h1 className="relative text-6xl text-cyan-500 font-bold mt-24 -mb-20 z-10">ProStock</h1>
+                <img src="/Prostock.png" alt="" className=" absolute w-80 left-1/2 transform -translate-x-44 -translate-y-1/2 opacity-35" />
+                <h1 className="relative text-8xl text-cyan-500 font-bold mt-28 -mb-28 z-10 font-['Afacad_Flux']">ProStock</h1>
             </div>
             <div className="flex">
                 <UserForm />
--- src/frontend/src/Pages/login.tsx
+++ src/frontend/src/Pages/login.tsx
@@ -3,8 +3,8 @@ export default function Login() {
     return (
         <>
             <div className="text-center">
-                <img src="/Prostock.png" alt="" className=" absolute w-52 left-1/2 right-96 transform -translate-x-28 -translate-y-1/2 opacity-35" />
-                <h1 className="relative text-6xl text-cyan-500 font-bold mt-24 -mb-32 z-10">ProStock</h1>
+                <img src="/Prostock.png" alt="" className="absolute w-80 left-1/2 transform -translate-x-44 -translate-y-1/2 opacity-35" />
+                <h1 className="relative text-8xl text-cyan-500 font-bold mt-28 -mb-48 z-10 font-['Afacad_Flux']">ProStock</h1>
             </div>
             <div className="flex">
                 <LoginForm /> {/* Substitui pelo componente de login */}
