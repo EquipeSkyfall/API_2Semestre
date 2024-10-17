761eb88d3cd256a324f04413ea5ec05e7132bded
Eduardo Fontes <160733714+DuuhZero@users.noreply.github.com>
2024-10-17 08:47:49 -0300

    Correção na medida da imagem ProStock

--- src/frontend/src/components/NavBar/index.tsx
+++ src/frontend/src/components/NavBar/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav className="bg-slate-50 flex items-center gap-12 p-4 mb-5 text-black border-b-slate-100 pb-0 -mt-2 shadow-md">
-      <img src="/Prostock.png" width={120}  className=" -mt-5 -mb-10" alt="Logo" />
+      <img src="/Prostock.png" width={120}  className=" -mt-10 -mb-5" alt="Logo" />
       <Link to='/products'>
         <button className="hover:border-b-4 hover:border-b-cyan-400 pb-2 bordernav">Produtos</button>
       </Link>
