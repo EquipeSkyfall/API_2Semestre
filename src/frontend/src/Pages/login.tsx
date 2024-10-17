047fa6612cf7ab1575f0444b62139e36bd3359de
Eduardo Fontes <160733714+DuuhZero@users.noreply.github.com>
2024-10-17 08:43:53 -0300

    Pagina Home com style atualizado e Login para entrada

--- src/frontend/src/Pages/login.tsx
+++ src/frontend/src/Pages/login.tsx
@@ -1,17 +1,14 @@
-import UserForm from "../components/UserForm";
-
-
-
-export default function Home() {
+import LoginForm from "../components/UserLogin";
+export default function Login() {
     return (
         <>
             <div className="text-center">
                 <img src="/Prostock.png" alt="" className=" absolute w-52 left-1/2 right-96 transform -translate-x-28 -translate-y-1/2 opacity-35" />
-                <h1 className="relative text-6xl text-cyan-500 font-bold mt-24 -mb-20 z-10">ProStock</h1>
+                <h1 className="relative text-6xl text-cyan-500 font-bold mt-24 -mb-32 z-10">ProStock</h1>
             </div>
             <div className="flex">
-                <UserForm />
+                <LoginForm /> {/* Substitui pelo componente de login */}
             </div>
         </>
     );
-}   
+}
