13f6ae1ca7fb9f3d3d2ebc2474f1d6f559245268
João Vitor Rossi <162117916+rossilindo@users.noreply.github.com>
2024-10-18 20:23:38 -0300

    Delete UserLogin directory

--- UserLogin/LoginSchema/loginSchema.tsx
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-const loginSchema = z.object({
-  email: z.string().email({ message: "O Email informado é inválido" }),
-  password: z.string().min(8, { message: "A senha informada é invalida" }),
-});
-
-export default loginSchema;
