af8f67bed7cb6a8376fca8005e76508374867a8e
João Vitor Rossi <162117916+rossilindo@users.noreply.github.com>
2024-10-18 20:28:52 -0300

    Delete CategoryForm directory

--- CategoryForm/CategorySchema/categorySchema.tsx
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const categorySchema = z.object({
-    nome_categoria: z.string().min(1, "Nome da categoria é obrigarório."),
-    descricao_categoria: z.string().optional()
-});
-
-export type CategorySchema = z.infer<typeof categorySchema>
