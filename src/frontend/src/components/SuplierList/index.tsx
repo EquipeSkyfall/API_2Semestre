6d6fef230ed4a4e56edd21beaf517dbb9f3d1af5
pauloalmeida46 <162117908+pauloalmeida46@users.noreply.github.com>
2024-10-19 19:26:38 -0300

    atualização do suplierlist para estilização

--- src/frontend/src/components/SuplierList/index.tsx
+++ src/frontend/src/components/SuplierList/index.tsx
@@ -57,38 +57,43 @@ const SupplierList: React.FC = () => {
     }
   }
 
-  if (isLoading) return <p>Loading suppliers...</p>;
-  if (isError) return <p>Error loading suppliers.</p>;
+  if (isLoading) return <p>Carregado Fornecedores...</p>;
+  if (isError) return <p>Erro ao tentar carregar os Fornecedores.</p>;
 
   const suppliers = data?.suppliers ?? [];
 
   return (
-    <div>
-      <h1>Supplier List</h1>
+    <div className='flex-1 p-5 bg-gray-100 rounded-lg shadow-md max-h-[80vh] overflow-y-auto box-border ml-10 mr-10 mt-8 mb-10'>
+      <h1 className='text-center'>Lista de Fornecedores</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="search-form">
+      <form onSubmit={handleSubmit(onSubmit)} className="search-form flex justify-center mb-5">
         <input
           {...register('search')}
           type="text"
-          placeholder="Search by Razão Social"
+          placeholder="Razão Social"
+          className='px-3 py-2'
         />
         <input
           {...register('cidade')}
           type="text"
-          placeholder="Filter by Cidade"
+          placeholder="Cidade"
+          className='px-3 py-2 ml-5'
         />
         <input
           {...register('estado')}
           type="text"
-          placeholder="Filter by Estado"
+          placeholder="Estado"
+          className='px-3 py-2 ml-5'
         />
-        <button type="submit">Search</button>
+        <button type="submit" className='px-3 py-2 bg-cyan-400 hover:bg-sky-400 text-white border-none 
+        rounded-md cursor-pointer text-base justify ml-5 '>Procurar</button>
       </form>
 
+      <div className='ml-[14%]'>
       {suppliers.length === 0 ? (
-        <p>No suppliers found.</p>
+        <p className='mt-3 ml-2 mb-4 flex justify-center'>Sem Fornecedores.</p>
       ) : (
-        <table className="supplier-table">
+        <table>
           <thead>
             <tr>
               <th>Razão Social</th>
@@ -97,7 +102,7 @@ const SupplierList: React.FC = () => {
               <th>Cidade</th>
               <th>Estado</th>
               <th>CEP</th>
-              <th>Actions</th>
+              <th>Ações</th>
             </tr>
           </thead>
           <tbody>
@@ -111,32 +116,49 @@ const SupplierList: React.FC = () => {
                 <td>{supplier.estado}</td>
                 <td>{supplier.cep}</td>
                 <td>
-                  <button onClick={() => openModal(supplier)}>Edit</button>
-                  <button
-                    onClick={() => handleDelete(supplier.id_fornecedor)}
-                  >Deletar
+                  <button className='px-2 py-1 bg-green-400 hover:bg-green-700 text-white border-none 
+                  rounded-md cursor-pointer text-base justify ml-2' onClick={() => openModal(supplier)}>
+                    Editar
                   </button>
-                  <button onClick={() => openProductModal(supplier)}>Add Products</button>
-                  <button onClick={() => openViewProductsModal(supplier)}>View Products</button>
+                  <button className='px-2 py-1 bg-red-400 hover:bg-red-700 text-white border-none 
+                  rounded-md cursor-pointer text-base justify ml-2' onClick={() => handleDelete(supplier.id_fornecedor)}>
+                    Deletar
+                  </button>
+                  <button className='px-2 py-1 bg-cyan-400 hover:bg-sky-400 text-white border-none 
+                  rounded-md cursor-pointer text-base justify ml-2' onClick={() => openProductModal(supplier)}>
+                      Add Produtos
+                  </button>
+                  <button className='px-2 py-1 bg-cyan-400 hover:bg-sky-400 text-white border-none 
+                  rounded-md cursor-pointer text-base justify ml-2' onClick={() => openViewProductsModal(supplier)}>
+                      Ver Produtos
+                  </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       )}
+      </div>
 
-      <div className="pagination">
-        <button onClick={handlePrevPage} disabled={page === 1}>
-          Previous
+      <div className="pagination ml-[40%] mr-[40%] mt-[40px]">
+        <button 
+          onClick={handlePrevPage} 
+          disabled={page === 1}
+          className='px-2 py-1 bg-cyan-400 hover:bg-sky-400 text-white border-none 
+        rounded-md cursor-pointer text-base justify mr-2 '
+        >
+          Anterior
         </button>
-        <span>
-          Page {data?.currentPage} of {data?.totalPages}
+        <span className='flex-1 p-1.5 max-h-[80vh] overflow-y-auto box-border'>
+          Página {data?.currentPage} de {data?.totalPages}
         </span>
         <button
           onClick={handleNextPage}
           disabled={page === data?.totalPages}
+          className='px-2 py-1 bg-cyan-400 hover:bg-sky-400 text-white border-none 
+        rounded-md cursor-pointer text-base justify ml-2'
         >
-          Next
+          Próximo
         </button>
       </div>
 
