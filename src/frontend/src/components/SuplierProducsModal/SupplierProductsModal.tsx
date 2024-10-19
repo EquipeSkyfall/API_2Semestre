bdc2bc1d00a6a588af20478940ca817e5f366952
pauloalmeida46 <162117908+pauloalmeida46@users.noreply.github.com>
2024-10-19 19:29:52 -0300

    atualização do supplierproductsmodal

--- src/frontend/src/components/SuplierProducsModal/SupplierProductsModal.tsx
+++ src/frontend/src/components/SuplierProducsModal/SupplierProductsModal.tsx
@@ -16,12 +16,11 @@ const SupplierProductsModal: React.FC<SupplierProductsModalProps> = ({
   isOpen,
   onClose,
 }) => {
-  const [page, setPage] = useState(1);
-  const { data, isLoading, isError } = useGetSupplierProducts(supplierId, {search: '', page,limit: 10});
+  const { data: products = [], isLoading, isError } = useGetSupplierProducts(supplierId);
   const { mutate: deleteProduct } = useDeleteProductFromSupplier();
   const [searchTerm, setSearchTerm] = useState('');
 
-  const filteredProducts = data?.products.filter((product) =>
+  const filteredProducts = products.filter((product) =>
     product.produto.nome_produto.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -32,66 +31,47 @@ const SupplierProductsModal: React.FC<SupplierProductsModalProps> = ({
     }
   };
 
-  const handleNextPage = () => setPage((prev) => prev + 1);
-  const handlePrevPage = () => setPage((prev) => Math.max(prev - 1, 1));
-
   if (!isOpen) return null;
 
   return (
     <div className="modal-overlay">
-        <div className="modal-content">
-            <h2>Products from Supplier</h2>
+      <div className="modal-content">
+        <h2>Produtos do Fornecedor</h2>
 
-            <input
-                type="text"
-                placeholder="Search products..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
+        <input
+          type="text"
+          placeholder="Procurando Produtos..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className='mt-2 mb-2'
+        />
 
-            {isLoading ? (
-                <p>Loading products...</p>
-            ) : isError ? (
-                <p>Error fetching products.</p>
-            ) : filteredProducts.length === 0 ? (
-                <p>No products found.</p>
-            ) : (
-                <>
-                    <ul>
-                        {filteredProducts.map((product) => (
-                            <li key={product.id_produto}>
-                                <div>
-                                    <strong>{product.produto.nome_produto}</strong>
-                                    <p>ID: {product.id_produto}</p>
-                                    <p>Preço de custo: {product.preco_custo}</p>
-                                    <button onClick={() => handleDelete(product.id_produto)}>
-                                        Delete
-                                    </button>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+        {isLoading ? (
+          <p className='mt-2 mb-2'>Carregando Produtos...</p>
+        ) : isError ? (
+          <p className='mt-2 mb-2'>Erro ao buscar Produtos.</p>
+        ) : filteredProducts.length === 0 ? (
+          <p className='mt-2 mb-2'>Sem Produtos.</p>
+        ) : (
+          <ul>
+            {filteredProducts.map((product) => (
+              <li key={product.id_produto}>
+                <div className='flex flex-row items-center justify-between p-4 bg-gray-100 rounded-md shadow-md mb-2'>
+                  <strong className="text-lg font-semibold text-gray-800">{product.produto.nome_produto}</strong>
+                  <p className="text-sm text-gray-600">ID: {product.id_produto}</p>
+                  <p className="text-sm text-gray-600">Preço de custo: {product.preco_custo}</p>
+                  <button className='px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded-md text-sm font-medium transition-colors duration-300'
+                  onClick={() => handleDelete(product.id_produto)}>
+                    Deletar
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
 
-                    {/* Pagination controls */}
-                    <div className="pagination">
-                        <button onClick={handlePrevPage} disabled={ data?.totalPages === 1}>
-                            Previous
-                        </button>
-                        <span>
-                            Page {page} of {data?.totalPages}
-                        </span>
-                        <button
-                            onClick={handleNextPage}
-                            disabled={page === data?.totalPages}
-                        >
-                            Next
-                        </button>
-                    </div>
-                </>
-            )}
-
-            <button onClick={onClose}>Close</button>
-        </div>
+        <button className='px-4 py-2 bg-cyan-400 hover:bg-sky-400 text-white rounded-md text-sm font-medium transition-colors duration-300 mt-5' onClick={onClose}>Fechar</button>
+      </div>
     </div>
   );
 };
