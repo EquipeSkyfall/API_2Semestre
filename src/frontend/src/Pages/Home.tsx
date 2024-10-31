23f4320953c1127af3432058ee6f5f570f06774d
DuuhZero <dudu4silvafontes@gmail.com>
2024-10-30 21:45:26 -0300

    Responsividade Login & Cadastro

--- src/frontend/src/Pages/Home.tsx
+++ src/frontend/src/Pages/Home.tsx
@@ -14,8 +14,8 @@ export default function Home() {
     return (
         <>
             <div className="text-center">
-                <img src="/Prostock.png" alt="" className=" absolute w-80 left-1/2 transform -translate-x-44 -translate-y-1/2 opacity-35" />
-                <h1 className="relative text-8xl text-cyan-500 font-bold mt-28 -mb-28 z-10 font-['Afacad_Flux']">ProStock</h1>
+                <img src="/Prostock.png" alt="" className="absolute w-80 left-1/2 transform -translate-x-44 -translate-y-1/2 sm:-translate-y-1/2 opacity-35" />
+                <h1 className="relative text-8xl sm:text-8xl text-cyan-500 font-bold mt-24 sm:mt-40 -mb-52  sm:-mb-40 z-10 font-['Afacad_Flux']">ProStock</h1>
             </div>
             <div className="flex">
                 <UserForm />
--- src/frontend/src/Pages/login.tsx
+++ src/frontend/src/Pages/login.tsx
@@ -13,11 +13,11 @@ export default function Login() {
     return (
         <>
             <div className="text-center">
-                <img src="/Prostock.png" alt="" className="absolute w-80 left-1/2 transform -translate-x-44 -translate-y-1/2 opacity-35" />
-                <h1 className="relative text-8xl text-cyan-500 font-bold mt-28 -mb-48 z-10 font-['Afacad_Flux']">ProStock</h1>
+                <img src="/Prostock.png" alt="" className="absolute w-80 left-1/2 transform -translate-x-44 -translate-y-1/3 sm:-translate-y-1/2 opacity-35" />
+                <h1 className="relative text-8xl sm:text-8xl text-cyan-500 font-bold mt-24 sm:mt-40 -mb-60 pt-12 sm:pt-0 z-10 font-['Afacad_Flux']">ProStock</h1>
             </div>
             <div className="flex">
-                <LoginForm /> {/* Substitui pelo componente de login */}
+                <LoginForm />
             </div>
         </>
     );
