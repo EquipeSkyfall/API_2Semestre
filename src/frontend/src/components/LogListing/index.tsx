727ce3d7df32c10a9b85749b1ad16779dc441d1e
jofran2001 <119539664+jofran2001@users.noreply.github.com>
2024-11-07 08:58:00 -0300

    ADD [FRONTEND] - Responsividade Histórico e Logs

--- src/frontend/src/components/LogListing/index.tsx
+++ src/frontend/src/components/LogListing/index.tsx
@@ -1,14 +1,45 @@
 import React, { useState, useEffect } from "react";
 import useGetLogs from "../../Hooks/Logs/getLogs";
+import { set } from "zod";
 
 const LogList: React.FC = () => {
     const [selectedDate, setSelectedDate] = useState<string>('');
     const [debouncedDate, setDebouncedDate] = useState<string>('');
     const [page, setPage] = useState(1);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const limit = 7;
+    const [limit, setLimit] = useState(7);
 
     const { data, isLoading, isError } = useGetLogs(debouncedDate, page, limit);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if(window.innerHeight <=668) {
+                setLimit(3)
+            } 
+            else if(window.innerWidth <=450) {
+                setLimit(5)
+            } 
+            else if (window.innerWidth <= 640) {
+                setLimit(7)
+            }
+            else if (window.innerWidth <= 666) {
+                setLimit(6);
+            }
+             else if (window.innerWidth <= 790) {
+                setLimit(7);
+            } else{
+                setLimit(10)
+            }
+        };
+
+        // Adiciona o listener e chama-o inicialmente
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     useEffect(() => {
         const handler = setTimeout(() => {
@@ -41,17 +72,18 @@ const LogList: React.FC = () => {
         <div>
             <button
                 onClick={openModal}
-                className="absolute top-32 right-6 px-5 py-3 bg-cyan-400 hover:bg-cyan-500   transition duration-300 text-white border-none rounded-md cursor-pointer">
+                className="md:px-5 md:py-3 px-10 py-2 md:text-base text-lg bg-cyan-400 hover:bg-cyan-500 transition duration-300 text-white border-none rounded-md cursor-pointer 
+                           md:absolute md:top-32 md:right-6 static top-24 mt-10 md:mt-0 ml-4">
                 Ver Logs
             </button>
 
             {isModalOpen && (
-                <div className="modal-overlay fixed inset-0 bg-black bg-opacity-50 flex !justify-end" onClick={closeModal}>
+                <div className="modal-overlay text-xs md:text-sm 2xl:text-base fixed inset-0 bg-black bg-opacity-50 flex 2xl:!justify-end " onClick={closeModal}>
                     <div
-                        className="modal-content justify-start items-end h-full p-6 fixed overflow-y-auto"
+                        className="modal-content !max-w-screen-md 2xl:!max-w-screen-md relative justify-start items-end h-5/6 2xl:h-full p-6 overflow-y-hidden"
                         onClick={(e) => e.stopPropagation()}>
                         <button className="close-button absolute top-4 right-4" onClick={closeModal}>X</button>
-                        <div className="overflow-">
+                        <div className="">
                             <h2 className="font-semibold font-['Afacad_Flux'] text-cyan-600 mb-4">Logs do Sistema</h2>
                             <div className="mb-4">
                                 <label htmlFor="datePicker" className="block font-medium mb-1">Selecione uma data:</label>
@@ -70,7 +102,7 @@ const LogList: React.FC = () => {
                         ) : isError ? (
                             <p>Erro ao Carregar Logs</p>
                         ) : (
-                            <table className="w-full">
+                            <table className="mr-2 sm:mr-0 sm:w-full text-[0.52rem] sm:text-sm md:text-sm 2xl:text-base "> 
                                 <thead>
                                     <tr>
                                         <th>Data do processo</th>
@@ -97,7 +129,7 @@ const LogList: React.FC = () => {
                         )}
         
                         {data && (
-                            <div className="flex justify-between items-center w-11/12 py-4 fixed bottom-0">
+                            <div className="flex justify-between items-center w-11/12 left-3 sm:left-auto py-2 sm:py-4 fixed bottom-0">
                                 <button
                                     disabled={page === 1}
                                     onClick={() => handlePageChange(page - 1)}
